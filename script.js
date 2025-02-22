03b0ad896c6e4a6633d5348642f3bc0dfcf46965
ILYAZAMNIUS97 <zamnius.ilya@yandex.ru>
2025-02-23 00:11:19 +0300

    Добавил блок "Мини-игры" и footer в документ

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-document.getElementById('go-button').addEventListener('click', function(event) {
-document.getElementById('Об_играх')
-.scrollIntoView({behavior: 'smooth' });});
+document.getElementById('go-button').addEventListener('click', function (event) {
+    document.getElementById('Об_играх')
+        .scrollIntoView({ behavior: 'smooth' });
+});
