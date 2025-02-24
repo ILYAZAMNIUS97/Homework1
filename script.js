b18220947e67435d74afb20bba1d4b9df15466e7
ILYAZAMNIUS97 <zamnius.ilya@yandex.ru>
2025-02-24 16:17:24 +0300

    Дорабатываю мобильную версию

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById('go-button').addEventListener('click', function (event) 
     document.getElementById('Об_играх')
         .scrollIntoView({ behavior: 'smooth' });
 });
+
+document.getElementById('go-button').addEventListener('click', function(event) {
+    event.preventDefault(); // Предотвращаем стандартное поведение формы
+    document.querySelector('.minigames').scrollIntoView({ behavior: 'smooth' }); // Плавный скролл
+});
